46809c945ca26820be8e288d31c11a74f94f8009
wingos80 <wingyc80@gmail.com>
2021-03-15 15:09:55 +0100

    made program run through a number of time steps, and plot the error from each interval

--- numerical ODE solver.py
+++ numerical ODE solver.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from copy import deepcopy
-from time import time
+import timeit
 
 l   = 1.            # (m) Length of pendulum
 g   = 9.81          # (m/s^2)
@@ -11,128 +11,211 @@ r   = 0.05          # (m) Radius of sphere
 A   = np.pi * r**2  # (m^2) Cross-secional area
 c_d = 0.47          # (.) Drag coefficient of a sphere
 
-alpha = c_d * rho * A / 2.   # Initially assume air-resitance is zero
-# alpha = 0
-T_final = 10000.   # (s) Final time
-N = 60000
+# alpha = c_d * rho * A / 2.   # Initially assume air-resitance is zero
+alpha = 0
 
-# (.) Number of time-steps
+T_final = 25     # (s) Final time
+N = 10           # (.) Number of time-steps
 dt = T_final / (N)
 
-yp_rk4 = 0
-y_rk4 = 1
+initial_conditions = [0,1]      #[yp,y]
+
+yp_rk4 = initial_conditions[0]
+y_rk4 = initial_conditions[1]
 y_history_rk4 = np.zeros(N+1)
 y_history_rk4[0] = y_rk4
 
-yp_fe_1 = 0
-y_fe_1 = 1
+yp_fe_1 = initial_conditions[0]
+y_fe_1 = initial_conditions[1]
 y_history_fe_1 = np.zeros(N+1)
 y_history_fe_1[0] = y_fe_1
 
-yp_fe_2 = 0
-y_fe_2 = 1
+yp_fe_2 = initial_conditions[0]
+y_fe_2 = initial_conditions[1]
 y_history_fe_2 = np.zeros(N+1)
 y_history_fe_2[0] = y_fe_2
 
 x = np.linspace(0, T_final, N+1)
 
+error_list = [[[],[]],[[],[]],[[],[]],[]]
+
+
 def dypdt(y, yp):
-    return (-m*g*np.sin(y)/l-alpha*l*yp*abs(l*yp))/m
-    # return -yp-y
+    # return (-m*g*np.sin(y)/l-alpha*l*yp*abs(l*yp))/m
+    return -yp-y
+    # return -y
 
 
 def y_exact(x):
     return 2/3*np.sqrt(3)*np.exp(-x/2)*np.sin(np.sqrt(3)/2*x+np.pi/3)
+    # return np.cos(x)
 
 
-def rk4(dypdt):
-    d1 = dt*yp_rk4
-    dp1 = dt*dypdt(y_rk4, yp_rk4)
-    d2 = dt*(yp_rk4+0.5*dp1)
-    dp2 = dt*dypdt(y_rk4+0.5*d1, yp_rk4+0.5*dp1)\
+def rk4(dypdt, dh):
+    d1 = dh*yp_rk4
+    dp1 = dh*dypdt(y_rk4, yp_rk4)
+    d2 = dh*(yp_rk4+0.5*dp1)
+    dp2 = dh*dypdt(y_rk4+0.5*d1, yp_rk4+0.5*dp1)\
         # (yp+0.5*dp1+y+0.5*d1)
-    d3 = dt*(yp_rk4+0.5*dp2)
-    dp3 = dt*dypdt(y_rk4+0.5*d2, yp_rk4+0.5*dp2)\
+    d3 = dh*(yp_rk4+0.5*dp2)
+    dp3 = dh*dypdt(y_rk4+0.5*d2, yp_rk4+0.5*dp2)\
         # (yp+0.5*dp2+y+0.5*d2)
-    d4 = dt*(yp_rk4+dp3)
-    dp4 = dt*dypdt(y_rk4+d3, yp_rk4+dp3)\
+    d4 = dh*(yp_rk4+dp3)
+    dp4 = dh*dypdt(y_rk4+d3, yp_rk4+dp3)\
         # (yp+dp3+y+d3)
     dy = 1/6*(d1+2*d2+2*d3+d4)
     dyp = 1/6*(dp1+2*dp2+2*dp3+dp4)
     return dy, dyp
 
 
-def fe(dypdt, yp):
+def fe(dypdt, yp, dh):
     ypp = dypdt(y_fe_1, yp)
-    dyp = ypp*dt
-    dy = yp*dt
+    dyp = ypp*dh
+    dy = yp*dh
     return dy, dyp
 
 
-def fe_inc(dypdt, yp1):
+def fe_inc(dypdt, yp1, dh):
     ypp = dypdt(y_fe_2, yp_fe_2)
-    dyp = ypp*dt
+    dyp = ypp*dh
     yp2 = yp1+dyp
-    dy = yp2*dt
+    dy = yp2*dh
     return dy, yp2
 
-print(N, "<--N")
-for i in range(1, N+1):
 
-    # ypp = (-m*g*np.sin(y)/l-alpha*l*yp*abs(l*yp))/m
-    # yp += ypp*dt
-    # ypp = -yp - y
-    # yp += ypp*dt
-    # y += yp*dt
-    dy_rk4, dyp_rk4 = rk4(dypdt)
-    y_rk4 += dy_rk4
-    yp_rk4 += dyp_rk4
-    y_history_rk4[i] = y_rk4
+for j in range(5):
+    toc = timeit.default_timer()
 
-    dy_fe, dyp_fe = fe(dypdt, yp_fe_1)
-    y_fe_1 += dy_fe
-    yp_fe_1 += dyp_fe
-    y_history_fe_1[i] = y_fe_1
+    N = 10**(j+1)
+    print(N, "<--number of steps")
+    dt = T_final / (N)
 
-    dy_fe_2, yp_fe_2 = fe_inc(dypdt, yp_fe_2)
-    y_fe_2 += dy_fe_2
-    y_history_fe_2[i] = y_fe_2
+    yp_rk4 = initial_conditions[0]
+    y_rk4 = initial_conditions[1]
+    y_history_rk4 = np.zeros(N + 1)
+    y_history_rk4[0] = y_rk4
 
-    # ypp_1 = (-m*g*np.sin(y_fe_1)/l-alpha*l*yp_fe_1*abs(l*yp_fe_1))/m
-    # y_fe_1 += yp_fe_1*dt
-    # yp_fe_1 += ypp_1*dt
-    # y_history_fe_1[i] = y_fe_1
-    #
-    # ypp_1 = -yp_fe_1-y_fe_1
-    # y_fe_1 += yp_fe_1*dt
-    # yp_fe_1 += ypp_1*dt
-    # y_history_fe_1[i] = y_fe_1
+    yp_fe_1 = initial_conditions[0]
+    y_fe_1 = initial_conditions[1]
+    y_history_fe_1 = np.zeros(N + 1)
+    y_history_fe_1[0] = y_fe_1
 
-    # ypp_2 = -yp_fe_2-y_fe_2
-    # yp_fe_2 += ypp_2*dt
-    # y_fe_2 += yp_fe_2*dt
-    # yp_his[i] = yp_fe_2
-    # y_history_fe_2[i] = y_fe_2
+    yp_fe_2 = initial_conditions[0]
+    y_fe_2 = initial_conditions[1]
+    y_history_fe_2 = np.zeros(N + 1)
+    y_history_fe_2[0] = y_fe_2
 
-    # ypp_2 = (-m*g*np.sin(y_fe_2)/l-alpha*l*yp_fe_2*abs(l*yp_fe_2))/m
-    # yp_fe_2 += ypp_2*dt
-    # y_fe_2 += yp_fe_2*dt
-    # y_history_fe_2[i] = y_fe_2
+    x = np.linspace(0, T_final, N + 1)
+
+    for i in range(1, N+1):
+        dy_rk4, dyp_rk4 = rk4(dypdt, dt)
+        y_rk4 += dy_rk4
+        yp_rk4 += dyp_rk4
+        y_history_rk4[i] = y_rk4
+
+        dy_fe, dyp_fe = fe(dypdt, yp_fe_1, dt)
+        y_fe_1 += dy_fe
+        yp_fe_1 += dyp_fe
+        y_history_fe_1[i] = y_fe_1
+
+        dy_fe_2, yp_fe_2 = fe_inc(dypdt, yp_fe_2, dt)
+        y_fe_2 += dy_fe_2
+        y_history_fe_2[i] = y_fe_2
+
+        # ypp_1 = (-m*g*np.sin(y_fe_1)/l-alpha*l*yp_fe_1*abs(l*yp_fe_1))/m
+        # y_fe_1 += yp_fe_1*dt
+        # yp_fe_1 += ypp_1*dt
+        # y_history_fe_1[i] = y_fe_1
+        #
+        # ypp_1 = -yp_fe_1-y_fe_1
+        # y_fe_1 += yp_fe_1*dt
+        # yp_fe_1 += ypp_1*dt
+        # y_history_fe_1[i] = y_fe_1
+
+        # ypp_2 = -yp_fe_2-y_fe_2
+        # yp_fe_2 += ypp_2*dt
+        # y_fe_2 += yp_fe_2*dt
+        # yp_his[i] = yp_fe_2
+        # y_history_fe_2[i] = y_fe_2
+
+        # ypp_2 = (-m*g*np.sin(y_fe_2)/l-alpha*l*yp_fe_2*abs(l*yp_fe_2))/m
+        # yp_fe_2 += ypp_2*dt
+        # y_fe_2 += yp_fe_2*dt
+        # y_history_fe_2[i] = y_fe_2
+    solution_plot_title = f"time step:{dt}"
+
+    index = -1
+    y_solution = y_exact(x)
+    rk4_error = abs(y_history_rk4-y_solution)
+    fe1_error = abs(y_history_fe_1-y_solution)
+    fe2_error = abs(y_history_fe_2-y_solution)
+    rk4_error_sum = 0
+    fe1_error_sum = 0
+    fe2_error_sum = 0
 
 
-y_solution = y_exact(x)
+    for i in range(len(y_history_fe_1)):
+        if i == 0 or i == len(y_history_fe_1)-1:
+            rk4_error_sum += 1 / 3 * dt * (rk4_error[i])
+            fe1_error_sum += 1 / 3 * dt * (fe1_error[i])
+            fe2_error_sum += 1 / 3 * dt * (fe2_error[i])
+        elif i % 2 == 1:
+            rk4_error_sum += 4 / 3 * dt * (rk4_error[i])
+            fe1_error_sum += 4 / 3 * dt * (fe1_error[i])
+            fe2_error_sum += 4 / 3 * dt * (fe2_error[i])
+        elif i % 2 == 0:
+            rk4_error_sum += 2 / 3 * dt * (rk4_error[i])
+            fe1_error_sum += 2 / 3 * dt * (fe1_error[i])
+            fe2_error_sum += 2 / 3 * dt * (fe2_error[i])
 
-# rk4_error = y_history_rk4-y_solution
-# fe1_error = y_history_fe_1-y_solution
-# fe2_error = y_history_fe_2-y_solution
+    error_list[0][0].append(np.log(abs(rk4_error[index]))/np.log(10))
+    error_list[0][1].append(np.log(abs(rk4_error_sum))/np.log(10))
+    error_list[1][0].append(np.log(abs(fe1_error[index]))/np.log(10))
+    error_list[1][1].append(np.log(abs(fe1_error_sum))/np.log(10))
+    error_list[2][0].append(np.log(abs(fe2_error[index]))/np.log(10))
+    error_list[2][1].append(np.log(abs(fe2_error_sum))/np.log(10))
+    error_list[3].append(abs(np.log(dt)/np.log(10)))
 
-plt.plot(x, y_history_rk4, label='4th order runge kutta')
-plt.plot(x, y_history_fe_1, label='forward euler *correct*')
-plt.plot(x, y_history_fe_2, label='forward euler *incorrect*')
+    tic = timeit.default_timer()
+    print(f"{tic-toc} <-- time taken for 10^{j+1} steps (step size = {dt})\n\n")
+
+fig, subfig = plt.subplots(2,2)
+fig.suptitle('stacked subplots')
+subfig[0][0].plot(x, y_history_rk4, label='4th order runge kutta')
+subfig[0][0].plot(x, y_history_fe_1, label='forward euler *correct*')
+subfig[0][0].plot(x, y_history_fe_2, label='forward euler *incorrect*')
+subfig[0][0].plot(x, y_solution, label='exact solution')
+subfig[0][0].legend(loc="upper right")
+subfig[0][0].set_ylabel('y', loc='center')
+subfig[0][0].set_xlabel('t', loc='left')
+subfig[0][0].set_title(f'Interval bound: {T_final}')
+subfig[0][0].grid()
+
+subfig[0][1].plot(error_list[3], error_list[0][0], label='4th order runge kutta', marker='x')
+subfig[0][1].plot(error_list[3], error_list[1][0], label='forward euler *correct*', marker='x')
+subfig[0][1].plot(error_list[3], error_list[2][0], label='forward euler *incorrect*', marker='x')
+subfig[0][1].legend(loc="upper right")
+subfig[0][1].set_ylabel('log10(error)', loc='center')
+subfig[0][1].set_xlabel('-log10(dt)', loc='left')
+subfig[0][1].set_title('error at interval end')
+subfig[0][1].grid()
+
+subfig[1][0].plot(error_list[3], error_list[0][1], label='4th order runge kutta', marker='x')
+subfig[1][0].plot(error_list[3], error_list[1][1], label='forward euler *correct*', marker='x')
+subfig[1][0].plot(error_list[3], error_list[2][1], label='forward euler *incorrect*', marker='x')
+subfig[1][0].legend(loc="upper right")
+subfig[1][0].set_ylabel('log10(Sigma error)', loc='center')
+subfig[1][0].set_xlabel('-log10(dt)', loc='left')
+subfig[1][0].set_title('integral of error')
+subfig[1][0].grid()
 # plt.plot(x, y_solution, label='exact solution')
 
 # plt.plot(x, rk4_error, label='4th order runge kutta error')
 # plt.plot(x, fe1_error, label='forward euler error *correct*')
 # plt.plot(x, fe2_error, label='forward euler error *incorrect*')
-plt.legend(loc="upper right")
+
+# print(error_list[3], '<-- -log10(dt)')
+
 plt.show()
+
+
